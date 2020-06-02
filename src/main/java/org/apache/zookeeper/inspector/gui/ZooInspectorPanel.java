38e1db97f98e0c95580e8b93626bd760bb30ea56
vergil <vergilchiu@foxmail.com>
2020-06-02 17:20:08 +0800

    jframe is setted center when opened

--- src/main/java/org/apache/zookeeper/inspector/gui/ZooInspectorPanel.java
+++ src/main/java/org/apache/zookeeper/inspector/gui/ZooInspectorPanel.java
@@ -142,6 +142,7 @@ public class ZooInspectorPanel extends JPanel implements
                         zooInspectorManager.getLastConnectionProps(),
                         zooInspectorManager.getConnectionPropertiesTemplate(),
                         ZooInspectorPanel.this);
+                zicpd.setLocationRelativeTo(null);
                 zicpd.setVisible(true);
             }
         });
